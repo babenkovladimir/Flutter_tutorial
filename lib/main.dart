8b52a83c4d0c4922f48b150fd3d32de5a3f11cee
vladimir <babenkovladimirbmd@gmail.com>
2019-01-20 18:33:19 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,1 @@
-import 'package:flutter/material.dart';
-import 'src/app.dart';
-
-void main() {
-//  var app = MaterialApp(
-//    home: Text('Hi  there!'),
-//  );
-
-  //var app = App();
-
-  runApp(App());
-}
+void main() {}
