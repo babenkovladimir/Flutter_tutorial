3c60266eb411ebe17d67410e56bb2d7e068eceb1
vladimir <babenkovladimirbmd@gmail.com>
2019-01-22 19:25:22 +0200

    mixins

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../mixins/validation_mixin.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -7,12 +9,17 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: <Widget>[emailFields(), passwordField(), Container(margin: EdgeInsets.only(top: 25.0)), submitButton()],
         ),
@@ -24,12 +31,22 @@ class LoginScreenState extends State<LoginScreen> {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(labelText: 'Email Address', hintText: 'you@example.com'),
+      validator: validateEmail,
+      onSaved: (String value) {
+        print(value);
+        email = value;
+      },
     );
   }
 
   Widget passwordField() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Enter Password', hintText: 'Password'),
+      validator: validatePassword,
+      onSaved: (String value) {
+        print(value);
+        password = value;
+      },
       //obscureText: true,
     );
   }
@@ -38,7 +55,12 @@ class LoginScreenState extends State<LoginScreen> {
     return RaisedButton(
       color: Colors.blue,
       child: Text('Submit'),
-      onPressed: () {},
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('time to post $email and $password');
+        }
+      },
     );
   }
 }
