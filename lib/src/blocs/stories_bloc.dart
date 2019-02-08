9305b8f052f90d1e90a157f13267672d72e37f67
vladimir <babenkovladimirbmd@gmail.com>
2019-02-08 10:46:12 +0200

    continue;

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,25 +1,57 @@
+import 'dart:async';
+
 import 'package:rxdart/rxdart.dart';
+
 import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
+  Observable<Map<int, Future<ItemModel>>> items; // Обсёрвабле мапа. И все буду обращаться только к ней!!!
 
   // Getters to get stream inside application
 
   // Getter to stream
   Observable<List<int>> get topIds => _topIds.stream;
+  // Don't do it this wil be removed
+  // Это плохо, потому что при каждом обращении мы будем обращаться к методу itemsTransformer и Создавать новую Map
+  //get items => _items.stream.transform(_itemsTransformer()); - Это был пример, который нельзя использовать;
 
+
+  // Getter for Sinks
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc(){
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, index) {
+        print('this is index $index');
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{}, // Initial value empty map
+    );
+  }
 
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
+
+/*
+* Каждый раз, когда мы создаём новый StoriesBloc, мы будем создавать мы будем описывать-создавать инстансь items, котрая
+* будет непосредственно мапой. Создавая контроллер для мы применяем для него трансформация только один раз и привязываем его к
+*
+* */
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,55 +1,64 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
 import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = StoriesProvider.of(context);
+
+    // This is bad!!!!! Dont doi this!
+    // temporary
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Top news"),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
   }
 
-//  Widget buildList(StoriesBloc bloc) {
-//    return StreamBuilder(
-//      stream: bloc.topIds,
-//      builder: (BuildContext context, snapshot) {
-//        if (snapshot.hasData) {
-//          return Text('Still waiting for ids');
-//        }
-//
-//        return ListView.builder(
-//          itemCount: snapshot.data.length,
-//          itemBuilder: (context, int index) {
-//            return Text(snapshot.data[index]);
-//          },
-//        );
-//      },
-//    );
-//  }
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          //return Text('Still waiting for ids');
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (BuildContext context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            return Container(
-              child: snapshot.hasData ? Text('Im visiba; $index') : Text('I havent fetched data yet @index'),
-              height: 80,
-            );
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            bloc.fetchItem(snapshot.data[index]);
+//            return Text('${snapshot.data[index]}');
+            return NewsListTile(itemId: snapshot.data[index],);
           },
         );
       },
     );
   }
+
+//  Widget buildList() {
+//    return ListView.builder(
+//      itemCount: 1000,
+//      itemBuilder: (BuildContext context, int index) {
+//        return FutureBuilder(
+//          future: getFuture(),
+//          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+//            return Container(
+//              child: snapshot.hasData ? Text('Im visiba; $index') : Text('I havent fetched data yet @index'),
+//              height: 80,
+//            );
+//          },
+//        );
+//      },
+//    );
+//  }
 
   getFuture() {
     // temporary test future.
